b71eb2fd30b5d7bce2e7d9593f4e97627f2d7c47
Artem Khan <akhan195@my.bcit.ca>
2024-05-07 13:43:14 -0700

    Removed unused code and files

--- placespeakISSP/csvimport/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.home, name="home"),
-    url(r'^download-data/$', views.download_data, name='download_data'),
-    url(r'^send-csv-to-api/$', views.send_csv_to_api, name='send_csv_to_api'),
-    url(r'^generate-analysis/$', views.generate_analysis, name='generate_analysis'),
-]
