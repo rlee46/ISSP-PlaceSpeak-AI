f7bd9273cab631f0e2cb4ba409e1329569c2b216
Sumit Khanduri <veerkhanduri@gmail.com>
2024-05-15 16:37:58 -0700

    fixed commas after merge

--- placespeakISSP/csvimport/utilities/helpers.py
+++ placespeakISSP/csvimport/utilities/helpers.py
@@ -8,19 +8,17 @@ class HelperFunctions:
         return len([line for line in lines if len(line) > 0])
 
     def csv_to_array(self, csv_data):
-        try:
-            data = []
-            reader = csv.DictReader(StringIO(csv_data))
-            skip_first_row = True
-            for row in reader:
-                if skip_first_row:
-                    skip_first_row = False
-                    continue
-                data.append(row)
-            return data
-        except Exception as e:
-            return e
-
+        
+        data = []
+        reader = csv.DictReader(StringIO(csv_data))
+        skip_first_row = True
+        for row in reader:
+            if skip_first_row:
+                skip_first_row = False
+                continue
+            data.append(row)
+        return data
+        
     def remove_non_printable_chars(self, text):
         printable_pattern = re.compile(r'[^\x20-\x7E]')
         return re.sub(printable_pattern, '', text)
